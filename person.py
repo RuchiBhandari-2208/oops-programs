5af7915b33ae379885e35a03acfe4ce0df985ad4
RuchiBhandari-2208 <ruchibhandari228@gmail.com>
2026-06-16 18:51:52 +0530

    create person.py

---
+++ person.py
@@ -0,0 +1,14 @@
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+        def display(self):
+         print( self.name)
+         print( self.age)
+
+p = person("Raj", 25)
+p.display()
+
+
+
